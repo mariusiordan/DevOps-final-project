9612198e26fc4855828b50f100edf016fb76b2a5
Marius Iordan <dopyno@yahoo.com>
2026-04-03 21:55:13 +0100

    feat: wire vpc-aws module into root main-aws.tf

--- aws-silverbank-ec2/terraform/main-aws.tf
+++ aws-silverbank-ec2/terraform/main-aws.tf
@@ -24,3 +24,18 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+# ------------------------------------------------------------
+# VPC
+# ------------------------------------------------------------
+
+module "vpc" {
+  source = "./modules/vpc-aws"
+
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+  project_name         = var.project_name
+  environment          = var.environment
+}
